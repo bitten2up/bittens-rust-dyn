ed1387428b5e2b90b01e7b6d8278a49bc911d3a6
bitten 1up <seant9140@gmail.com>
2022-12-12 18:37:58 -0600

    add gitignore

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
+//use std::thread;
+//use std::time::Duration;
+
 #[repr(C)]
+/// struct of bit_settings
 pub struct bit_settings {
-    pub width: cty::c_int,
-    pub height: cty::c_int,
-    pub audio: bool,
-    pub modded: bool,
-    pub silent: bool, // dont show modded text durring gameplay
+    pub width: cty::c_int,  // width of window
+    pub height: cty::c_int, // height of window
+    pub audio: bool,        // do we play audio
+    pub modded: bool,       // sets game modified, this is set after the function is run
+    pub silent: bool,       // dont show modded text durring gameplay
 }
+/// this is the patching function
 #[no_mangle]
-pub extern "C" fn bittenEnginePatch(settings: *mut bit_settings) -> cty::c_int {
+pub extern "C" fn bittenEnginePatch(settings: *mut bit_settings) {
 	unsafe {
-	(*settings).width=900;
+	    (*settings).width=900; // show that it worked
+        (*settings).silent=false; // get rid of modded text
+        
 	}
-	println!("Bitten engine patching worked");
-	return 1;
+    let text=format!("Bitten engine patching worked.\nvalue of settings.width {}", unsafe{(*settings).width});
+    println!("{}", text);
+    return;
 }
